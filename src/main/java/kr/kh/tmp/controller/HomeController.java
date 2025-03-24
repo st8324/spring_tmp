78bf53705c9883ca1e5737ce7daa2e485015f5dc
st8324 <stajun@naver.com>
2025-03-24 12:11:02 +0900

    타일즈 템플릿 적용

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -1,8 +1,12 @@
 package kr.kh.tmp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.tmp.service.MemberService;
 
 @Controller
 public class HomeController {
@@ -15,5 +19,4 @@ public class HomeController {
 		
 		return "home";
 	}
-	
 }
--- src/main/webapp/WEB-INF/views/common/footer.jsp
+++ src/main/webapp/WEB-INF/views/common/footer.jsp
@@ -7,6 +7,8 @@
 
 </head>
 <body>
-	<h1>메인 페이지입니다.</h1>
+	<div class="jumbotron">
+	    <h1>푸터</h1>      
+	</div>
 </body>
 </html>
