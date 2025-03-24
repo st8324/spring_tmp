1655504f60f8389e824e1ba16d6bb882479e9317
st8324 <stajun@naver.com>
2025-03-24 16:17:50 +0900

    로그아웃 구현 완료

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -1,5 +1,7 @@
 package kr.kh.tmp.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -57,6 +59,16 @@ public class HomeController {
 			model.addAttribute("url", "/login?id=" + member.getMe_id());
 			model.addAttribute("msg", "로그인에 실패했습니다.");
 		}
+		return "message";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(Model model, HttpSession session) {
+		
+		session.removeAttribute("user");
+		
+		model.addAttribute("url", "/");
+		model.addAttribute("msg", "로그아웃 했습니다.");
 		return "message";
 	}
 	
