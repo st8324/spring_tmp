e43e31cbaf8a3e4a79a0a16b7b7267c220c4b932
st8324 <stajun@naver.com>
2025-03-24 11:06:28 +0900

    jik2와 병합

--- src/main/java/kr/kh/tmp/controller/HomeController.java
+++ src/main/java/kr/kh/tmp/controller/HomeController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private MemberService memberService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		
